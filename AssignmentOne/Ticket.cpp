e2c48be0c0989047970a1d98906de9b3c29c048c
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-09-13 15:15:24 +0300

    Implement Ticket class methods
    Implemented the following methods in Ticket.cpp:
    - Constructor to initialize passenger details and seat booking.
    - Getter methods for accessing passenger, flight, and seat information.

--- AssignmentOne/Ticket.cpp
+++ AssignmentOne/Ticket.cpp
@@ -1,1 +1,28 @@
 #include "Ticket.h"
+
+Ticket::Ticket(const std::string &passengerName, const std::string &flightNumber, const std::string &date, int row, int seat)
+        : passengerName(passengerName), flightNumber(flightNumber), date(date), row(row), seat(seat), isBooked(true) {}
+
+std::string Ticket::getPassengerName() const {
+    return passengerName;
+}
+
+std::string Ticket::getFlightNumber() const {
+    return flightNumber;
+}
+
+std::string Ticket::getDate() const {
+    return date;
+}
+
+int Ticket::getRow() const {
+    return row;
+}
+
+int Ticket::getSeat() const {
+    return seat;
+}
+
+bool Ticket::getBookingStatus() const {
+    return isBooked;
+}
