cc2bacb0330dc07b27c487660fea6eed0cd14b10
Yuriy Klishch <yuriyklyshch@gmail.com>
2024-09-13 15:13:36 +0300

    Define Ticket class structure
    Added the Ticket class structure in Ticket.h:
    - Declared attributes for passenger name, flight number, seat number, and booking status.
    - Defined the constructor and getter methods.
    
    No implementation added yet.

--- AssignmentOne/Ticket.h
+++ AssignmentOne/Ticket.h
@@ -1,4 +1,26 @@
 #ifndef ASSIGNMENTONE_TICKET_H
 #define ASSIGNMENTONE_TICKET_H
 
+#include <string>
+
+class Ticket {
+private:
+    std::string passengerName;
+    std::string flightNumber;
+    std::string date;
+    int row;
+    int seat;
+    bool isBooked;
+
+public:
+    Ticket(const std::string &passengerName, const std::string &flightNumber, const std::string &date, int row, int seat);
+
+    std::string getPassengerName() const;
+    std::string getFlightNumber() const;
+    std::string getDate() const;
+    int getRow() const;
+    int getSeat() const;
+    bool getBookingStatus() const;
+};
+
 #endif
